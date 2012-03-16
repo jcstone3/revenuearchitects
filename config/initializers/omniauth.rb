ff4955da25fe7b687330ecda8f649000a06d5345
nidhi.sarvaiya <nidhi.savaiya@icicletech.com>
2012-03-16 13:03:28 +0530

    modified facebook api key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,9 +5,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
  # provider :google_apps, 'dev.revenue-grader.com', '687896268286.apps.googleusercontent.com', 'qJ3gKLSbbeAT3-ZShMsArias'
 
  #facebook
-  provider :facebook, '389187014443203', '3f0e1b1d0965ad0a8d4a0694677ddf2f'#,
+  #provider :facebook, '389187014443203', '3f0e1b1d0965ad0a8d4a0694677ddf2f'#,
   #         {:scope => 'email, read_stream, read_friendlists, friends_likes, friends_status, offline_access',
   #         :client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
   #twitter
-   provider :twitter, '0MCcStPgM8eAvlIpDZEU5g', 'NTxxbMxIXqsj2m3DWBAiosJPQp7IcDtLBd4l9nKOY'
+  # provider :twitter, '0MCcStPgM8eAvlIpDZEU5g', 'NTxxbMxIXqsj2m3DWBAiosJPQp7IcDtLBd4l9nKOY'
 end
