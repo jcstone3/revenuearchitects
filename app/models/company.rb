eb12a44af49898b2b1ad53512a8c541f93e77de1
rajeshwar.chauhan <rajeshwar.chauhan@icicletech.com>
2012-03-17 13:08:32 +0530

    Added validations for company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,11 @@
 class Company < ActiveRecord::Base
-	validates :name, :presence => true
-	validates :website, :presence => true
+	validates :name, :presence => true, :format => { :with => /\A[a-zA-Z]+\z/,
+    		  :message => "Only letters allowed" }, :length => { :maximum => 30 }
+
+    
+	validates :website, :presence => true, :length => { :maximum => 30 },
+			  :format => {:with => /^[a-zA-Z\d:\.\/]*$/i,:message =>"invalid url"} 
+	
 	validates :user_id, :presence => true
     validates :industry_id, :presence => true, :numericality => true
     
