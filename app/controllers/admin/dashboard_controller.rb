02c982ec9db0b4082c8d986d572c802fc9e0ae29
nidhi.sarvaiya <nidhi.savaiya@icicletech.com>
2012-04-05 15:44:34 +0530

    added logic for dashboard page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,16 @@ class Admin::DashboardController < Admin::ApplicationController
    layout 'admin'
    before_filter :authenticate_admin!
 	def index
+		@total_surveys = Survey.find(:all, 
+						 :select => "count(id) as total, to_char(start_date, 'd') as survey_day",
+						 :conditions => "start_date between date_trunc('month', current_date) and date_trunc('month', current_date)+'1month'::interval-'1day'::interval",
+						 :group => "survey_day")
+
+		@total = Survey.find(:all, 
+							 :select => "count(id) as total",
+							  :conditions => "start_date between date_trunc('month', current_date) and date_trunc('month', current_date)+'1month'::interval-'1day'::interval"
+							).first.total
+		
 	end	
 
 	def show
