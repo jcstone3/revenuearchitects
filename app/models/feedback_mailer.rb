83832d4520f3e1a57399b8f90ce324a3f06311e8
srinivas.elluri <srinivas.elluri@icicletech.com>
2012-04-27 14:33:57 +0530

    admin page layouts

--- app/models/feedback_mailer.rb
+++ app/models/feedback_mailer.rb
@@ -3,8 +3,8 @@ class FeedbackMailer < ActionMailer::Base
   def feedback(feedback)
     recipients  = 'admin@revenuegrader.com'
     subject     = "[Feedback for YourSite] #{feedback.subject}"
-
+    from 		= "#{feedback.email}"
     @feedback = feedback
-    mail(:to => recipients, :subject => subject)
+    mail(:to => recipients, :subject => subject, :from => from)
   end
 end
