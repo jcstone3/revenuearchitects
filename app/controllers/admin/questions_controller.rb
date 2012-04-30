ffad45544fa4af8ce2fd332322a0d663d9fb587a
rashmi.nair <rashmi.nair@icicletech.com>
2012-04-30 10:08:47 +0530

    question createa modified admin module

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -20,15 +20,17 @@ class Admin::QuestionsController < ApplicationController
 	def create		
 		@question = Question.new(params[:question])		
         if @question.save
-          @sub_section = SubSection.find(@question.sub_section_id)
-          @section = Section.find(@sub_section.section_id)
-          @section_question = @section.question_count += 1
-          @section_total = @section.total_points += @question.points
-          if @section.update_attributes(:question_count => @section_question ,:total_points => @section_total)
+          #find the section for the subsection to increase the count of the 
+          #the questions added to that section          
+          @section = Section.find(:all, :joins=>"sub_sections on sections.id = sub_sections.section_id and sub_sections.id = @question.sub_section_id")
+          @section_question_count = @section.question_count += 1
+          @section_total_points = @section.total_points + @question.points
+
+          if @section.update_attributes(:question_count => @section_question_count ,:total_points => @section_total_points)
           flash[:success] = "Question Created successfully"
           redirect_to :action => 'index'
           else
-            flash[:success] = "Question Created successfully"
+            flash[:success] = "Question could not successfully"
             redirect_to :action => 'index'
           end  
         else
@@ -49,8 +51,10 @@ class Admin::QuestionsController < ApplicationController
    @question = Question.find(params[:id])
    
     if @question.update_attributes(params[:question])
-       @questions =   Question.find(:all, :select => "sum(questions.points) as question_points, count(*) as question_count, sections.name", :joins => "inner join sub_sections on questions.sub_section_id = sub_sections.id inner join sections on sections.id = sub_sections.section_id", :group => "sections.name")
+      #get questions count and questions total point for each section 
+       @questions =   Question.find(:all, :select => "sum(questions.points) as question_points, count(*) as question_count, sections.name", :joins => "left join sub_sections on questions.sub_section_id = sub_sections.id inner join sections on sections.id = sub_sections.section_id", :group => "sections.name")
        @sections = Section.all
+
         @sections.each_with_index do |section, i|
           section.update_attributes(:total_points=> @questions[i].question_points, :question_count=>@questions[i].question_count)
         end 
