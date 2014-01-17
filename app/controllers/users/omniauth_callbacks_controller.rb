eaad6d27ee40af0af008454012c2b560c43cc9eb
chandravati.gupta <chandravati.gupta@icicletech.com>
2014-01-17 19:29:07 +0530

    Added oauth2 method.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -47,9 +47,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
  
   def google_oauth2
     # @user = User.find_for_oauth(request.env["omniauth.auth"], current_user, "twitter")
-     data = request.env["omniauth.auth"].extra.raw_info
+     #data = request.env["omniauth.auth"].extra.raw_info
     #if @user.persisted?
-      logger.debug "User #{data} is persisted"
+      #logger.debug "User #{data} is persisted"
     #if new user then create Authorization
     #  authentication = Authorization.find_by_provider_and_uid('twitter', "#{data.id}")
     #  if authentication
@@ -61,8 +61,18 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     #else
     #  logger.error "User #{@user.id} is not persisted"
     #  session["devise.twitter_data"] = request.env["omniauth.auth"]
+      #redirect_to new_user_registration_url
+   # end 
+
+    @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user, "google")
+
+    if @user.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+      sign_in_and_redirect @user, :event => :authentication
+    else
+      session["devise.google_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
-   # end   
+    end
   end  
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -43,6 +43,18 @@ def self.find_for_oauth(access_token, signed_in_resource=nil, provider)
     end
   end
 
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil, provider)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+
+    unless user
+        user = User.create(name: data["name"],
+             email: data["email"],
+             password: Devise.friendly_token[0,20]
+            )
+    end
+    user
+  end
   def self.new_with_session(params, session)
     super.tap do |user|
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
