d5fadd3bf7a426530d0fdc0ab76a301f210898ed
Rohit Bhore <rohit.bhore@icicletech.com>
2014-03-14 15:41:19 +0530

    merge landing page branch, resolve merge conflicts.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,21 +1,22 @@
 class SiteController < ApplicationController
-	layout "website"
-  
+  layout "layout_website", except: :index
+  layout "website", only: :index
+
   def index
     if user_signed_in?
-     redirect_to new_survey_url  
+     redirect_to new_survey_url
     elsif admin_signed_in?
-     redirect_to  admin_root_url 
+     redirect_to  admin_root_url
     else
-    end  
+    end
   end
 
   def aboutus
-    
+
   end
 
   def contactus
-       
+
   end
 
   def privacy_policy
