cd1bae4f411fdf82e2179375c59bdebb9331ccd4
Rashmi Nair <rashmi.nair@icicletech.com>
2014-05-21 15:08:12 +0530

    [contactus form] add support Id

--- app/mailers/usermailer.rb
+++ app/mailers/usermailer.rb
@@ -33,7 +33,7 @@ class Usermailer < ActionMailer::Base
 
     # @body = body`enter code here`
 
-    mail(:to => "rohit.bhore@icicletech.com", :subject => "RevenueGrader: Contact Request", :tag => 'contact-request') do |format|
+    mail(:to => "rohit.bhore@icicletech.com, support.revenuegrader@icicletech.com", :subject => "RevenueGrader: Contact Request", :tag => 'contact-request') do |format|
       format.html { render "usermailer/contactus" }
     end
 
