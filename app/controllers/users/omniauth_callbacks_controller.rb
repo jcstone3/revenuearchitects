bdca9ecae29511901e5990895feb5fda0ed5e552
Claudio Ceballos Paz <claudiocebpaz@gmail.com>
2014-09-25 14:57:39 -0300

    Add Login OATH with LinkedIn App

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -45,7 +45,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end 
  
-  def google_oauth2   
+  def google_oauth2
     @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user, "google_oauth2")
     data = request.env["omniauth.auth"].extra.raw_info
     
@@ -56,6 +56,26 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       session["devise.google_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
-  end  
+  end
+
+  def linkedin
+    @user = User.find_for_oauth(request.env["omniauth.auth"], current_user, "linkedin")
+    data = request.env["omniauth.auth"].extra.raw_info
+    if @user.persisted? 
+    logger.debug "User #{data} is persisted"
+      #if new user then create Authorization
+      authentication = Authorization.find_by_provider_and_uid('linkedin', data.id)
+      if authentication
+       else
+         @user.authorizations.create!(:provider => 'linkedin', :uid => @user.username, :link => data.link) 
+      end
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "LinkedIn"
+      sign_in_and_redirect @user, :event => :authentication
+    else
+      logger.error "User #{@user.email} is not persisted"
+      session["devise.linkedin_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
 
 end
