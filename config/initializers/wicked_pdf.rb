8e23a0323661ece5ec7fe507734b7b75e94763e5
Rafael Camacho <rafael@softwarecriollo.com>
2015-10-26 17:12:31 -0430

    wicked pdf initializers modifications

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,23 +1,43 @@
 module WickedPdfHelper
   def wicked_pdf_stylesheet_link_tag(*sources)
     sources.collect { |source|
-      "<style type='text/css'>#{Rails.application.assets.find_asset("#{source}.css.erb")}</style>"
-    }.join("\n").gsub(/url\(['"](.+)['"]\)(.+)/,%[url("#{wicked_pdf_image_location("\\1")}")\\2]).html_safe
+      "<style type='text/css'>#{Rails.application.assets.find_asset(source+".css.erb")}</style>"
+    }.join("\n").html_safe
   end
 
   def wicked_pdf_image_tag(img, options={})
-    image_tag wicked_pdf_image_location(img), options
+    asset = Rails.application.assets.find_asset(img)
+    image_tag "file://#{asset.pathname.to_s}", options
   end
 
-  def wicked_pdf_image_location(img)
-    "file://#{Rails.root.join('app', 'assets', 'images', img)}"
-  end
-
-  def wicked_pdf_javascript_src_tag(source)
-    "<script type='text/javascript'>#{Rails.application.assets.find_asset("#{source}.js").body}</script>"
+  def wicked_pdf_javascript_src_tag(jsfile, options={})
+    asset = Rails.application.assets.find_asset(jsfile)
+    javascript_include_tag "file://#{asset.pathname.to_s}", options
   end
 
   def wicked_pdf_javascript_include_tag(*sources)
-    sources.collect{ |source| wicked_pdf_javascript_src_tag(source) }.join("\n").html_safe
+    sources.collect{ |source| "<script type='text/javascript'>#{Rails.application.assets.find_asset(source+".js")}</script>" }.join("\n").html_safe
+  end
+
+  module Assets
+    def wicked_pdf_stylesheet_link_tag(*sources)
+      sources.collect { |source|
+        "<style type='text/css'>#{Rails.application.assets.find_asset(source+".css.erb")}</style>"
+      }.join("\n").html_safe
+    end
+
+    def wicked_pdf_image_tag(img, options={})
+      asset = Rails.application.assets.find_asset(img)
+      image_tag "file://#{asset.pathname.to_s}", options
+    end
+
+    def wicked_pdf_javascript_src_tag(jsfile, options={})
+      asset = Rails.application.assets.find_asset(jsfile)
+      javascript_include_tag "file://#{asset.pathname.to_s}", options
+    end
+
+    def wicked_pdf_javascript_include_tag(*sources)
+      sources.collect{ |source| "<script type='text/javascript'>#{Rails.application.assets.find_asset(source+".js")}</script>" }.join("\n").html_safe
+    end
   end
 end
